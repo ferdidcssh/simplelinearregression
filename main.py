c4e11caef963a336a38ced2c8ae526e57a4ed04e
ferdidcssh <ferdidcssh@gmail.com>
2025-12-10 13:45:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-# 1 import library
-from sklearn.linear_model import LinearRegression
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 import streamlit as st
+import matplotlib.pyplot as plt
 
-# 2 menyiapkan data
-X = [[1], [2], [3], [4]] #tipe data list 2 dimensi
-y = [101, 102, 103, 104] #tipe data list 1 dimensi
+df = pd.read_csv('indonesian_salary_by_region.csv')
 
-# 3 instansiasi objek
+st.title("Prediksi Gaji UMP/UMK Indonesia")
+st.markdown("---")
+
+region = df['REGION'].unique()
+
+input_region = st.selectbox("Pilih Provinsi", options= region)
+
+df_selected = df[df['REGION'] == input_region].copy()
+
+X = df_selected[['YEAR']] # independent 
+y = df_selected['SALARY'] # dependent
+
 model = LinearRegression()
-
-st.title("Prediksi Gaji")
-
-# 4 training
 model.fit(X, y)
-input_user = st.number_input("Masukan value :")
 
-prediction = model.predict([[input_user]])
+min_year = df_selected['YEAR'].min()
+max_year = df_selected['YEAR'].max()
+input_year = st.number_input("Masukan Tahun: ", min_value = min_year,  value = max_year+1)
 
-# 6 visualisasi
+prediction = model.predict([[input_year]])
+
+formated_salary = f"Rp {prediction[0]:,.0f}".replace(",", "_").replace(".",",").replace("_", ".")
+
+st.metric( label=f'Prediksi Gaji di Provinsi {input_region} di Tahun {input_year}',value= formated_salary)
+
+st.subheader("Grafik Prediksi")
+
 fig, ax = plt.subplots()
 
-prediksi_y = model.predict(X)
-ax.scatter(X, y)
-ax.plot(X, prediksi_y)
-ax.scatter([input_user], [prediction])
+ax.scatter(X, y, label=f'Data Historis UMP/UMK {input_region} 1997-2025', color='blue')
+
+ax.plot(X, model.predict(X), color='red', label='Garis Regresi Linear')
+
+ax.scatter([input_year], [prediction], color='green', label=f'Prediksi tahun {input_year}')
+
+ax.set_xlabel("Tahun")
+ax.set_ylabel("Gaji")
+ax.set_title(f'Tren Kenaikan gaji di {input_region}')
+ax.legend()
 
 st.pyplot(fig)
-st.metric(label="Gaji", value=prediction)
